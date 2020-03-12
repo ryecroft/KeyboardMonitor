dac09a77e63a864c18cceb6a06467317963c794c
Stephen Horne <fatste@gmail.com>
2020-03-12 14:36:17 +0100

    Remove default test to allow build

--- Tests/KeyboardMonitorTests/KeyboardMonitorTests.swift
+++ Tests/KeyboardMonitorTests/KeyboardMonitorTests.swift
@@ -6,7 +6,7 @@ final class KeyboardMonitorTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(KeyboardMonitor().text, "Hello, World!")
+//        XCTAssertEqual(KeyboardMonitor().text, "Hello, World!")
     }
 
     static var allTests = [
